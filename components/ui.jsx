e698735f56f5220638206ba2897a46506a39b195
matiasmaldonado526 <matiasmaldonado526@gmail.com>
2026-01-07 15:44:02 +0000

    🐛bug(UI)arreglo de bugs y colocacion de imagenes

--- components/ui.jsx
+++ components/ui.jsx
@@ -3,9 +3,9 @@ import { View, Text } from 'react-native';
 import { PanGestureHandler } from 'react-native-gesture-handler'; 
 import Animated, { useAnimatedStyle } from 'react-native-reanimated';
 
-import { images } from '../model/images.model'; // Modelo de las imágenes
-import { ImageCard } from './ImageCard'; // Componente que muestra las imágenes
-import { useCarouselController } from '../controller/useCarouselController'; // El hook
+import { images } from '@/lib/Imagenes'; // Modelo de las imágenes
+import { ImageCard } from '@/components/Prin.jsx'; // Componente que muestra las imágenes
+import { useCarouselController } from '@/lib/logit'; // El hook
 
 export const CarouselView = () => {
   // Usamos el hook para obtener la lógica de los gestos
@@ -21,15 +21,15 @@ export const CarouselView = () => {
       <Text className="text-xl font-bold mb-4">Desliza las imágenes</Text>
 
       {/* Gestor del gesto de arrastre (PanGestureHandler) */}
-      <PanGestureHandler onGestureEvent={gestureHandler}>
+      Gesture.Pan(
+        onGestureEvent={gestureHandler}
         {/* Aquí animamos la vista que contiene las imágenes */}
         <Animated.View style={animatedStyle} className="flex-row">
           {images.map((img) => (
             // Renderizamos las imágenes con el componente ImageCard
             <ImageCard key={img.id} uri={img.uri} />
           ))}
-        </Animated.View>
-      </PanGestureHandler>
+        </Animated.View>) 
     </View>
   );
 };
